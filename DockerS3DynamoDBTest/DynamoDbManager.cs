e0b32d6a913f12ef1f61d2b6a63bc7470fa38b6e
Gleison Gibellato da Silva <ggibellato@gmail.com>
2023-07-21 20:09:55 +0100

    Test dynamodb 1

---
+++ DockerS3DynamoDBTest/DynamoDbManager.cs
@@ -0,0 +1,78 @@
+﻿using Amazon.DynamoDBv2.Model;
+using Amazon.DynamoDBv2;
+
+namespace DockerS3DynamoDBTest
+{
+    internal class DynamoDbManager
+    {
+        public AmazonDynamoDBClient Client => _client;
+
+        private readonly AmazonDynamoDBClient _client;
+
+        public DynamoDbManager(string accessKeyId, string secretAccessKey, Amazon.RegionEndpoint region, string serviceUrl)
+        {
+            AmazonDynamoDBConfig config = new AmazonDynamoDBConfig
+            {
+                RegionEndpoint = region
+            };
+            if(!string.IsNullOrWhiteSpace(serviceUrl))
+            {
+                config.ServiceURL = "http://localhost:8000";
+            }
+            _client = new AmazonDynamoDBClient(accessKeyId, secretAccessKey, config);
+        }
+
+        public async Task CreateTableIfNotExists(string tableName)
+        {
+            if (!await TableExists(tableName))
+            {
+                await CreateNewTable(tableName);
+            }
+            else
+            {
+                Console.WriteLine($"Table '{tableName}' already exists. Skipping table creation.");
+            }
+        }
+
+        private async Task<bool> TableExists(string tableName)
+        {
+            var response = await _client.ListTablesAsync();
+            return response.TableNames.Contains(tableName);
+        }
+
+        private async Task CreateNewTable(string tableName)
+        {
+            var request = new CreateTableRequest
+            {
+                TableName = tableName,
+                AttributeDefinitions = new List<AttributeDefinition>
+            {
+                new AttributeDefinition
+                {
+                    AttributeName = "primaryKey", // Replace with your primary key attribute name
+                    AttributeType = ScalarAttributeType.S // Assuming it's a string attribute, you can change this as needed
+                }
+                // You can add more attribute definitions for other attributes.
+            },
+                KeySchema = new List<KeySchemaElement>
+            {
+                new KeySchemaElement
+                {
+                    AttributeName = "primaryKey", // Replace with your primary key attribute name
+                    KeyType = KeyType.HASH
+                }
+                // You can add more key schema elements for other attributes as needed.
+            },
+                ProvisionedThroughput = new ProvisionedThroughput
+                {
+                    ReadCapacityUnits = 5, // Adjust these values as needed based on your requirements
+                    WriteCapacityUnits = 5
+                }
+            };
+
+            var response = await _client.CreateTableAsync(request);
+            Console.WriteLine($"Table '{tableName}' has been created. Status: {response.TableDescription.TableStatus}");
+        }
+    }
+
+}
--- DockerS3DynamoDBTest/UnitTest1.cs
+++ DockerS3DynamoDBTest/UnitTest1.cs
@@ -1,11 +1,51 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using FluentAssertions;
+
 namespace DockerS3DynamoDBTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        private DynamoDbManager _dynamoDbManager;
+        private string _tableName = "Test1";
 
+        public UnitTest1 () {
+            _dynamoDbManager = new DynamoDbManager("fake", "fake", 
+                Amazon.RegionEndpoint.EUWest1, "http://localhost:8000");
+            
+        }
+
+
+
+        [Fact]
+        public async void DynamoDBTest()
+        {
+            await _dynamoDbManager.CreateTableIfNotExists(_tableName);
+            var primaryKey = "PK1";
+            var data = "{}";
+
+            var requestPut = new PutItemRequest
+            {
+                TableName = _tableName,
+                Item = new Dictionary<string, AttributeValue>{
+                    { "primaryKey", new AttributeValue { S = primaryKey } },
+                    { "data", new AttributeValue { S = data } }
+                    }
+            };
+
+            var responsePut = await _dynamoDbManager.Client.PutItemAsync(requestPut);
+            responsePut.Should().NotBeNull();
+
+            var requestGet = new GetItemRequest
+            {
+                TableName = _tableName,
+                Key = new Dictionary<string, AttributeValue> {
+                    { "primaryKey", new AttributeValue { S = primaryKey } }
+                }       
+            };
+
+            var responseGet = await _dynamoDbManager.Client.GetItemAsync(requestGet);
+            responseGet.Should().NotBeNull();
         }
     }
 }
